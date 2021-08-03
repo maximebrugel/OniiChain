82ca64291783764538d09b00f35e15ba39524137
maximebrugel <maxime.brugel@gmail.com>
2021-08-03 12:45:11 +0200

    feat: Add DetailCaller library

--- contracts/libraries/DetailCaller.sol
+++ contracts/libraries/DetailCaller.sol
@@ -1,4 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-library DetailCaller {}
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+/// @title Helper to call detail libraries
+library DetailCaller {
+    /// @notice Call the library item function
+    /// @param lib The library address
+    /// @param id The item ID
+    function getDetailSVG(address lib, uint8 id) external returns (string memory) {
+        (bool success, bytes memory data) = lib.delegatecall(
+            abi.encodeWithSignature(string(abi.encodePacked("item_", Strings.toString(id), "()")))
+        );
+        require(success);
+        return abi.decode(data, (string));
+    }
+}
