31e931b3e7d2c408d488975cdb0034ce6ec3ed66
maximebrugel <maxime.brugel@gmail.com>
2021-08-10 17:53:27 +0200

    feat: Expression implementations

--- contracts/libraries/details/ExpressionDetail.sol
+++ contracts/libraries/details/ExpressionDetail.sol
@@ -12,23 +12,65 @@ library ExpressionDetail {
         return "";
     }
 
-    /// @dev Expressions N°2 => Blasé
+    /// @dev Expressions N°2 => Blush Cheeks
     function item_2() internal pure returns (string memory) {
         return
             base(
                 string(
                     abi.encodePacked(
-                        '<line stroke="#000000" stroke-linecap="round" stroke-miterlimit="10" x1="254.6" y1="158.2" x2="254.6" y2="188.8"/>',
-                        '<line stroke="#000000" stroke-linecap="round" stroke-miterlimit="10" x1="257.6" y1="158.2" x2="257.6" y2="188.8"/>',
-                        '<line stroke="#000000" stroke-linecap="round" stroke-miterlimit="10" x1="260.6" y1="162.8" x2="260.6" y2="193.3"/>',
-                        '<line stroke="#000000" stroke-linecap="round" stroke-miterlimit="10" x1="265" y1="159.8" x2="265" y2="190.4"/>'
+                        '<g opacity="0.71">',
+                        '<ellipse fill="#FF7478" cx="257.6" cy="221.2" rx="11.6" ry="3.6"/>',
+                        '<ellipse fill="#FF7478" cx="146.9" cy="221.5" rx="9.6" ry="3.6"/>',
+                        "</g>"
                     )
                 )
             );
     }
 
-    /// @dev Expressions N°3 => Sweet
+    /// @dev Expressions N°3 => Blush
     function item_3() internal pure returns (string memory) {
+        return
+            base(
+                string(
+                    abi.encodePacked(
+                        '<ellipse opacity="0.52" fill-rule="evenodd" clip-rule="evenodd" fill="#FF7F83" cx="196.8" cy="222" rx="32.8" ry="1.9"/>'
+                    )
+                )
+            );
+    }
+
+    /// @dev Expressions N°4 => Dark Circle
+    function item_4() internal pure returns (string memory) {
+        return
+            base(
+                string(
+                    abi.encodePacked(
+                        '<path d="M160.1,223.2c4.4,0.2,8.7-1.3,12.7-3.2C169.3,222.7,164.4,223.9,160.1,223.2z"/>',
+                        '<path d="M156.4,222.4c-2.2-0.4-4.3-1.6-6.1-3C152.3,220.3,154.4,221.4,156.4,222.4z"/>',
+                        '<path d="M234.5,222.7c4.9,0.1,9.7-1.4,14.1-3.4C244.7,222.1,239.3,223.4,234.5,222.7z"/>',
+                        '<path d="M230.3,221.9c-2.5-0.4-4.8-1.5-6.7-2.9C225.9,219.9,228.2,221,230.3,221.9z"/>'
+                    )
+                )
+            );
+    }
+
+    /// @dev Expressions N°5 => Blase
+    function item_5() internal pure returns (string memory) {
+        return
+            base(
+                string(
+                    abi.encodePacked(
+                        '<line fill="none" stroke="#000000" stroke-linecap="round" stroke-miterlimit="10" x1="254.6" y1="158.2" x2="254.6" y2="188.8"/>',
+                        '<line fill="none" stroke="#000000" stroke-linecap="round" stroke-miterlimit="10" x1="257.6" y1="158.2" x2="257.6" y2="188.8"/>',
+                        '<line fill="none" stroke="#000000" stroke-linecap="round" stroke-miterlimit="10" x1="260.6" y1="162.8" x2="260.6" y2="193.3"/>',
+                        '<line fill="none" stroke="#000000" stroke-linecap="round" stroke-miterlimit="10" x1="265" y1="159.8" x2="265" y2="190.4"/>'
+                    )
+                )
+            );
+    }
+
+    /// @dev Expressions N°6 => Sweat
+    function item_6() internal pure returns (string memory) {
         return
             base(
                 string(
@@ -41,8 +83,8 @@ library ExpressionDetail {
             );
     }
 
-    /// @dev Expressions N°4 => Angry
-    function item_4() internal pure returns (string memory) {
+    /// @dev Expressions N°7 => Angry
+    function item_7() internal pure returns (string memory) {
         return
             base(
                 string(
@@ -56,7 +98,26 @@ library ExpressionDetail {
             );
     }
 
-    // TODO ADD OTHER ITEMS
+    /// @notice Return the expression name of the given id
+    /// @param id The expression Id
+    function getItemNameById(uint8 id) internal pure returns (string memory name) {
+        name = "";
+        if (id == 1) {
+            name = "Expressionless";
+        } else if (id == 2) {
+            name = "Blush Cheeks";
+        } else if (id == 3) {
+            name = "Blush";
+        } else if (id == 4) {
+            name = "Dark Circle";
+        } else if (id == 5) {
+            name = "Blase";
+        } else if (id == 6) {
+            name = "Sweat";
+        } else if (id == 7) {
+            name = "Angry";
+        }
+    }
 
     /// @dev The base SVG for the eyes
     function base(string memory children) private pure returns (string memory) {
