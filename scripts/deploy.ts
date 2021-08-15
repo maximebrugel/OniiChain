52be5d475d467cb498b0a64c665177fa0b955429
maximebrugel <maxime.brugel@gmail.com>
2021-08-15 12:11:30 +0200

    feat: deploy contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,93 @@
+import hre from "hardhat";
+
+const delay = async (ms: number) => new Promise(res => setTimeout(res, ms));
+
 async function main(): Promise<void> {
-  console.log("Nothing to deploy yet");
+  console.log("Deploy OniiChain");
+
+  let accessoryDetailFactory = await hre.ethers.getContractFactory("AccessoryDetail");
+  let backgroundDetailFactory = await hre.ethers.getContractFactory("BackgroundDetail");
+  let bodyDetailFactory = await hre.ethers.getContractFactory("BodyDetail");
+  let earringsDetailFactory = await hre.ethers.getContractFactory("EarringsDetail");
+  let expressionDetailFactory = await hre.ethers.getContractFactory("ExpressionDetail");
+  let eyebrowDetailFactory = await hre.ethers.getContractFactory("EyebrowDetail");
+  let eyesDetailFactory = await hre.ethers.getContractFactory("EyesDetail");
+  let hairDetailFactory = await hre.ethers.getContractFactory("HairDetail");
+  let mouthDetailFactory = await hre.ethers.getContractFactory("MouthDetail");
+  let noseDetailFactory = await hre.ethers.getContractFactory("NoseDetail");
+  let tatooDetailFactory = await hre.ethers.getContractFactory("TatooDetail");
+
+  let accessoryDetail = await deployLibrary(accessoryDetailFactory);
+  let backgroundDetail = await deployLibrary(backgroundDetailFactory);
+  let bodyDetail = await deployLibrary(bodyDetailFactory);
+  let earringsDetail = await deployLibrary(earringsDetailFactory);
+  let expressionDetail = await deployLibrary(expressionDetailFactory);
+  let eyebrowDetail = await deployLibrary(eyebrowDetailFactory);
+  let eyesDetail = await deployLibrary(eyesDetailFactory);
+  let hairDetail = await deployLibrary(hairDetailFactory);
+  let mouthDetail = await deployLibrary(mouthDetailFactory);
+  let noseDetail = await deployLibrary(noseDetailFactory);
+  let tatooDetail = await deployLibrary(tatooDetailFactory);
+
+  let hypnosisDescriptorFactory = await hre.ethers.getContractFactory("HypnosisDescriptor", {
+    libraries: {
+      AccessoryDetail: accessoryDetail.address,
+      BackgroundDetail: backgroundDetail.address,
+      BodyDetail: bodyDetail.address,
+      EarringsDetail: earringsDetail.address,
+      ExpressionDetail: expressionDetail.address,
+      EyebrowDetail: eyebrowDetail.address,
+      EyesDetail: eyesDetail.address,
+      HairDetail: hairDetail.address,
+      MouthDetail: mouthDetail.address,
+      NoseDetail: noseDetail.address,
+      TatooDetail: tatooDetail.address,
+    },
+  });
+  let hypnosisDescriptor = await hypnosisDescriptorFactory.deploy();
+  await hypnosisDescriptor.deployed();
+  await delay(60000);
+  await hre.run("verify:verify", {
+    address: hypnosisDescriptor.address,
+    libraries: {
+      AccessoryDetail: accessoryDetail.address,
+      BackgroundDetail: backgroundDetail.address,
+      BodyDetail: bodyDetail.address,
+      EarringsDetail: earringsDetail.address,
+      ExpressionDetail: expressionDetail.address,
+      EyebrowDetail: eyebrowDetail.address,
+      EyesDetail: eyesDetail.address,
+      HairDetail: hairDetail.address,
+      MouthDetail: mouthDetail.address,
+      NoseDetail: noseDetail.address,
+      TatooDetail: tatooDetail.address,
+    },
+  });
+
+  // Deploy Hypnosis
+  let hypnosisFactory = await hre.ethers.getContractFactory("Hypnosis");
+  let hypnosis = await hypnosisFactory.deploy(hypnosisDescriptor.address);
+  await hypnosis.deployed();
+  await delay(60000);
+  await hre.run("verify:verify", {
+    address: hypnosis.address,
+    constructorArguments: [hypnosisDescriptor.address],
+  });
+
+  console.log("Deploy done");
+}
+async function deployLibrary(factory: any) {
+  let contract = await factory.deploy();
+  await contract.deployed();
+  await delay(60000);
+  await simpleEtherscan(contract.address);
+  return contract;
+}
+
+async function simpleEtherscan(addressDeployed: string) {
+  await hre.run("verify:verify", {
+    address: addressDeployed,
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere and properly handle errors.
